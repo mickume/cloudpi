f4a4bc326c914ea5155175d0c51aef28aa233234
Michael Kuehl <michael@ratchet.cc>
2019-12-11 16:18:35 +0100

    changed the way app server starts

--- deployments/hivemq-ce/run.sh
+++ deployments/hivemq-ce/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo ""
-echo "  HiveMQ Start Script for Linux/Unix v1.10"
+echo "HiveMQ Start Script for Linux/Unix v1.10"
 echo ""
 
 JAVA_OPTS="$JAVA_OPTS -Djava.net.preferIPv4Stack=true"
@@ -14,6 +14,8 @@ JAVA_OPTS="$JAVA_OPTS --add-opens jdk.management/com.sun.management.internal=ALL
 JAVA_OPTS="$JAVA_OPTS --add-exports java.base/jdk.internal.misc=ALL-UNNAMED"
 JAVA_OPTS="$JAVA_OPTS -Djava.security.egd=file:/dev/./urandom"
 JAVA_OPTS="$JAVA_OPTS -XX:+CrashOnOutOfMemoryError"
+JAVA_OPTS="$JAVA_OPTS -Djava.util.prefs.systemRoot=${APP_ROOT}/.java -Djava.util.prefs.userRoot=${APP_ROOT}/.java/.userPrefs"
+
 # JMX Monitoring
 # JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 HIVEMQ_HOME_OPTS="-Dhivemq.home=${HIVEMQ_HOME}"
